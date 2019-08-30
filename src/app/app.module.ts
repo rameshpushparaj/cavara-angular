0519e02a24ddcf6a052fe6fc4a2bf66f94e2f501
Ramesh Pushparaj <ramesh.vlb08@gmail.com>
2019-08-30 21:01:31 +0530

    Angular7 App Sample

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    CarouselComponent,
+    GalleryComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
